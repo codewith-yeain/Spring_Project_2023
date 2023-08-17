d1168add97c15d82485e3e01128fc903f44ffd1e
codewith-yeain <dev.yeain@gmail.com>
2023-08-17 12:39:50 +0900

    Mapper 어노테이션 추가, css 경로 수정, 컨트롤러 작업 진행, 로그인/회원가입/비밀번호 찾기 퍼블 수정

--- src/main/java/com/app/icontact/controller/MemberController.java
+++ src/main/java/com/app/icontact/controller/MemberController.java
@@ -1,11 +1,30 @@
 package com.app.icontact.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member/*")
 public class MemberController {
+    @GetMapping("join")
+    public void join(){
+        log.info("join.................");
+    }
+
+    @GetMapping("login")
+    public void login(){
+        log.info("login.................");
+    }
+
+    @GetMapping("find-password")
+    public void findPassword(){
+        log.info("find-password.................");
+    }
+
+
 }
---
+++ src/test/java/com/app/icontact/mapper/MemberMapperTests.java
@@ -0,0 +1,30 @@
+package com.app.icontact.mapper;
+
+import com.app.icontact.domain.MemberVO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class MemberMapperTests {
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    public void insertTest(){
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
